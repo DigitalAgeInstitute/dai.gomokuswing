9af6c44237a243a2f0807f859ad9937c24f45fe8
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2013-06-12 16:50:55 +0300

    removed the deleted files from being tracked

--- src/dai/gomoku/client/swing/requests/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Holds the clients Request objects
- * @author Muriithi Frederick Muriuki
- *
- */
-package dai.gomoku.client.swing.requests;
--- src/dai/gomoku/client/swing/responses/package-info.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * This package deals with the responses from the server
- * 
- * @author Muriithi Frederick Muriuki
- *
- */
-package dai.gomoku.client.swing.responses;
